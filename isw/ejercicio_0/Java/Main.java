f7e45674e3b27b32aa19b5b5bc307af6068e131d
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2022-08-24 13:21:34 -0500

    orginize folders

--- isw/ejercicio_0/Java/Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Edgar");
-    System.out.println("Ingenieria en Computacion");
-    System.out.println("Ceti Colomos");
-    System.out.println("Me apasiona la computacion");
-    System.out.println("C, C++, Java, Javascript, Python, Rust");
-  }
-}
--- isw/ejercicio_0/Python/Main.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    print("Edgar")
-    print("Ingenieria en Computacion")
-    print("Ceti Colomos")
-    print("Me apasiona la computacion")
-    print("C, C++, Java, Javascript, Python, Rust")
