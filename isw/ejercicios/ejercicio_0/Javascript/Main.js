2139b508c2dd0d2401eb7fee4f935492b7d76ad4
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2022-10-07 00:28:38 -0500

    remove unnecessary files

--- isw/ejercicios/ejercicio_0/Javascript/Main.js
+++
@@ -1,6 +0,0 @@
-console.log("Edgar");
-console.log("Ingenieria en Computacion");
-console.log("Ceti Colomos");
-console.log("Me apasiona la computacion");
-console.log("C, C++, Java, Javascript, Python, Rust");
-
--- isw/ejercicios/ejercicio_0/Rust/Main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!("Edgar");
-    println!("Ingenieria en Computacion");
-    println!("Ceti Colomos");
-    println!("Me apasiona la computacion");
-    println!("C, C++, Java, Javascript, Python, Rust");
-}
